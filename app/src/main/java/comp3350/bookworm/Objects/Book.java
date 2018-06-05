8fd97f95d5edea566b987648f97ee42f0dd23f73
HaeSoo Kim <kimhs@myumanitoba.ca>
2018-06-04 22:18:21 -0500

    Revert "Merge branch 'book_description' into 'master'"
    This reverts merge request !1

--- app/src/main/java/comp3350/bookworm/Objects/Book.java
+++ app/src/main/java/comp3350/bookworm/Objects/Book.java
@@ -2,37 +2,4 @@ package comp3350.bookworm.Objects;
 
 public class Book {
     // Hold the info of each book
-
-    private final String bookName;
-    private final String authorName;
-    private final String bookPreview;
-    private final double bookPrice;
-
-    public Book( String bookName, String authorName, String bookPreview, double bookPrice ) {
-        this.bookName = bookName;
-        this.authorName = authorName;
-        this.bookPreview = bookPreview;
-        this.bookPrice = bookPrice;
-    }
-
-    public String getBookName() {
-        return bookName;
-    }
-
-    public String getAuthorName() {
-        return authorName;
-    }
-
-    public String getPreview() {
-        return bookPreview;
-    }
-
-    public double getBookPrice() {
-        return bookPrice;
-    }
-
-    public String toStirng() {
-        return bookName + authorName + bookPreview + bookPrice;
-    }
-
 }
