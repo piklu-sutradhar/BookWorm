1a2f6d5b28e844187b7b0c1bbc14608aae753e68
Mingo <lim34521@myumanitoba.ca>
2018-06-06 10:28:21 -0500

    Add book list on HomePage

--- app/src/main/java/comp3350/bookworm/Presentation/HomePage.java
+++ app/src/main/java/comp3350/bookworm/Presentation/HomePage.java
@@ -7,16 +7,22 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.widget.ListView;
 import android.widget.SearchView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
+import comp3350.bookworm.Application.Service;
 import comp3350.bookworm.BusinessLogic.AccountManager;
+import comp3350.bookworm.BusinessLogic.BookAdapter;
 import comp3350.bookworm.Objects.Account;
+import comp3350.bookworm.Objects.Book;
 import comp3350.bookworm.R;
 
 public class HomePage extends AppCompatActivity {
 
-    private TextView mTextMessage;
+//    private TextView mTextMessage;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -44,7 +50,7 @@ public class HomePage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
 
-        mTextMessage = (TextView) findViewById(R.id.home_page_message);
+//        mTextMessage = (TextView) findViewById(R.id.home_page_message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.home_page_navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
@@ -55,6 +61,14 @@ public class HomePage extends AppCompatActivity {
                 searchView.clearFocus();
             }
         }, 300);
+
+        // Construct the data source
+        ArrayList<Book> books = Service.getBookListStub().getBookList();
+        // Create the adapter to convert the array to views
+        BookAdapter bookAdapter = new BookAdapter(this, books);
+        // Attach the adapter to a ListView
+        ListView listView = (ListView) findViewById(R.id.bookList);
+        listView.setAdapter(bookAdapter);
 
     }
 
