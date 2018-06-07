2ef7f85e6498506c259c3980fecba44b31fdb3b9
HaeSoo Kim <kimhs@myumanitoba.ca>
2018-06-06 20:32:02 -0500

    remove test codes in DescriptionActivity.java

--- app/src/main/java/comp3350/bookworm/Presentation/DescriptionActivity.java
+++ app/src/main/java/comp3350/bookworm/Presentation/DescriptionActivity.java
@@ -11,7 +11,6 @@ import android.widget.TextView;
 import comp3350.bookworm.R;
 
 public class DescriptionActivity extends AppCompatActivity {
-    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
