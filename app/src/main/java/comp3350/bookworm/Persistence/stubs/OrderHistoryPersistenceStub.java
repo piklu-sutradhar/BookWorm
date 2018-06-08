4738f5411d84b2f3db3b210b5afafbfe7d90403a
gg KaoChi <heq3456@myumanitoba.ca>
2018-06-07 20:17:47 -0500

    add order history to sub

---
+++ app/src/main/java/comp3350/bookworm/Persistence/stubs/OrderHistoryPersistenceStub.java
@@ -0,0 +1,6 @@
+package comp3350.bookworm.Persistence.stubs;
+
+import comp3350.bookworm.Persistence.OrderHistoryPersistence;
+
+public class OrderHistoryPersistenceStub implements OrderHistoryPersistence {
+}
